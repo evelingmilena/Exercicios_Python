e9f0dfa1c0b5526acc4ce04262976f649369de40
evelingmilena <evelingmilena@usp.br>
2022-06-22 19:07:44 -0300

    Colocando aula nova e exercicio novo

---
+++ aula4.py
@@ -0,0 +1,14 @@
+x = 1
+
+while x<10:
+    print(x)
+    x=x+1
+
+#O laco for e como se fosse o while. No entanto, nao e necessario inicializar a variavel e nem atualizar o valor da mesma.  
+for x in range(10): #Significa: para x no intervalo de 0 a 9
+    print(x)   
+
+for n in range(5,15): #Significa: para n no intervalo de 5 a 14
+    dn=2*n
+    print("O dobro de",n,"e",dn)    
+
--- exercicio2.py
+++ exercicio2.py
@@ -1,24 +1,22 @@
 """
 Esse aqui e o codigo para decomposicao em numeros primos.
 
-O teorema fundamental da aritmetica diz que todo numero inteiro positivo e maior que 1 pode ser escrito ou decomposto em multiplicação de numeros primos.
 """
 
 
 
-# verificacao do teorema fundamental da aritmetica
 
-n = 20
-i = 1
-j = 1
 
-if n>0 and n>1 and type(n)==int:
+n = 100000000000
+i = 2
+j = n
+
+if n>1 and type(n)==int:
     print("Esse numero pode ser decomposto em fatores primos.")
-    while i<10:
-        while j<10:
-            if j%i==0 and j%1==j:
-                print(j)
-            j = j + 1
+    while j>1:
+        while j%i==0:
+            print(i)
+            j = j/i
         i = i + 1           
 elif type(n)!=int:
     print("O numero dever ser do tipo inteiro.")
