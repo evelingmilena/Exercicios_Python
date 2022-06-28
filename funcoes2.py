6cc4e164dbf53dd08b8451e6ced2a207080b0e65
evelingmilena <evelingmilena@usp.br>
2022-06-28 18:34:09 -0300

    Aula sobre funcoes

---
+++ funcoes2.py
@@ -0,0 +1,9 @@
+def myfunction(fname,lname):
+    print("Meu nome e",fname,lname)
+    
+myfunction("Eveling","Ribeiro")
+myfunction("Marcos","Ribeiro")
+myfunction("Joao","Barretos")
+myfunction("Carlos","Otobone")
+
+    
